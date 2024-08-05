dc11e3ebf8578da36973d255c05f7182cc205c33
JanSavitskiy <savitskiy.log@gmail.com>
2024-08-05 18:25:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sqlite3
 import telebot
 from telebot import types
-
-
+import requests
 
 
 TOKEN = "YOUR_TOKEN"  # Замените на токен вашего бота
 bot = telebot.TeleBot(TOKEN)
+
 
 # Создание базы данных и таблицы
 def create_database():
@@ -18,7 +18,7 @@ def create_database():
         )
     ''')
     conn.commit()
-    conn.close()
+
 
 # Добавление пользователя в базу данных
 def add_user(user_id):
@@ -35,10 +35,12 @@ answers = "Перепроверь ещё раз свой запрос"  # Соо
 @bot.message_handler(commands=["start"])
 def welcome(message):                       # Функция welcome и главные кнопки меню
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True) # Какая-то фишка для работы кнопок                
-    button3 = types.KeyboardButton('Поиск по коду')
-    button4 = types.KeyboardButton('ТГК с фильмами')
+    button1 = types.KeyboardButton('Поиск по жанрам')
+    button2 = types.KeyboardButton('Поиск по коду')
+    button3 = types.KeyboardButton('ТГК с фильмами')
     button5 = types.KeyboardButton('Настройки')           # Вывод кнопок на экран пользователю
-    markup.row(button3, button4, button5)
+    markup.row(button1, button2, button3)
+    markup.row(button5)
     user_id = message.from_user.id
     add_user(user_id)
 
@@ -47,7 +49,6 @@ def welcome(message):                       # Функция welcome и глав
         bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}!\nМеня зовут ФИЛЬМОБОТ и я твой проводник в мир кино и сериалов :)\nДля начала выбери способ поиска фильма в меню ниже: ', reply_markup=markup)  # Приветственное сообщение пользователю, не забываем про markup
     else:
         bot.send_message(message.chat.id, 'Перекинул тебя в главное меню! Выбирай!', reply_markup=markup) # если пользователь по своему желанию зашёл в меню
-
 
 
 @bot.message_handler(commands=['users_admin'])
@@ -74,42 +75,105 @@ def info(message):
         tgk(message)  # Регистрируем следующий шаг
     elif message.text == 'Настройки':
         settings(message)
-def search(message):
-        n = int(message.text)  # Получаем число из сообщения
-        my_dict = {
-            1: {
-                "Название": "Интерстеллар",
-                "Описание": "Космическая одиссея о группе астронавтов, которые исследуют червоточину в поисках нового дома для человечества.",
-                "Ссылка": "https://www.imdb.com/title/tt0816692/"
-            },
-            2: {
-                "Название": "Начало",
-                "Описание": "Вор, специализирующийся на краже идей, получает задание внедриться в сознание человека и изменить его мысли.",
-                "Ссылка": "https://www.imdb.com/title/tt1375666/"
-            },
-            3: {
-                "Название": "Матрица",
-                "Описание": "Программист Нейо узнает о правде о своей реальности и о том, что он может изменить мир.",
-                "Ссылка": "https://www.imdb.com/title/tt0133093/"
-            },
-            4: {
-                "Название": "Побег из Шоушенка",
-                "Описание": "История о дружбе двух заключенных, которые пытаются сбежать из тюрьмы Шоушенк.",
-                "Ссылка": "https://www.imdb.com/title/tt0111161/"
-            },
-            5: {
-                "Название": "Сияние",
-                "Описание": "Писатель с семьей становится смотрителем у отеля, где начинают происходить странные события.",
-                "Ссылка": "https://www.imdb.com/title/tt0081505/"
-            }
-        }
+    elif message.text == 'Поиск по жанрам':
+        bot.send_message(message.chat.id, "Введите жанр фильма:\nP.s вводите жанр правильно\nДоступные жанры: Драма, Ужасы, Фантастика, Комедия")
+        bot.register_next_step_handler(message, searchGenre)
+    elif message.text == '↩️ Назад в меню':
+        welcome(message)
+    else:
+        bot.send_message(message.chat.id, answers)
 
+my_dict = {
+    1: {
+        "Название": "Интерстеллар",
+        "Описание": "Космическая одиссея о группе астронавтов, которые исследуют червоточину в поисках нового дома для человечества.",
+        "Ссылка": "https://www.imdb.com/title/tt0816692/",
+        "Жанр": "Фантастика"
+    },
+    2: {
+        "Название": "Начало",
+        "Описание": "Вор, специализирующийся на краже идей, получает задание внедриться в сознание человека и изменить его мысли.",
+        "Ссылка": "https://www.imdb.com/title/tt1375666/",
+        "Жанр": "Фантастика"
+    },
+    3: {
+        "Название": "Матрица",
+        "Описание": "Программист Нейо узнает о правде о своей реальности и о том, что он может изменить мир.",
+        "Ссылка": "https://www.imdb.com/title/tt0133093/",
+        "Жанр": "Фантастика"
+    },
+    4: {
+        "Название": "Побег из Шоушенка",
+        "Описание": "История о дружбе двух заключенных, которые пытаются сбежать из тюрьмы Шоушенк.",
+        "Ссылка": "https://www.imdb.com/title/tt0111161/",
+        "Жанр": "Драма"
+    },
+    5: {
+        "Название": "Сияние",
+        "Описание": "Писатель с семьей становится смотрителем у отеля, где начинают происходить странные события.",
+        "Ссылка": "https://www.imdb.com/title/tt0081505/",
+        "Жанр": "Ужасы"
+    }, 
+    6: {
+        "Название": "Тупой и еще тупее",
+        "Описание": "После того, как женщина оставляет портфель в терминале аэропорта, тупой водитель лимузина и его еще более тупой друг отправляются в веселую поездку по пересеченной местности в Аспен, чтобы вернуть его.",
+        "Ссылка": "https://www.imdb.com/title/tt0109686/?ref_=ls_t_1",
+        "Жанр" : "Комедия"
+    }
+}
+
+def search(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton('↩️ Назад в меню')
+    markup.row(button1)
+    
+    try:
+        n = int(message.text)  # Получаем число из сообщения
+        
         if n in my_dict:
             movie = my_dict[n]
             response = f"Название: {movie['Название']}\nОписание: {movie['Описание']}\nСсылка: {movie['Ссылка']}"
-            bot.send_message(message.chat.id, response)
+            bot.send_message(message.chat.id, response, reply_markup=markup)
         else:
-            bot.send_message(message.chat.id, "Фильм не найден. Пожалуйста, попробуйте другой номер.")
+            bot.send_message(message.chat.id, "Фильм не найден. Пожалуйста, попробуйте другой номер.", reply_markup=markup)
+
+    except ValueError:
+        bot.send_message(message.chat.id, "Пожалуйста, введите корректное числовое значение.")
+
+def searchGenre(message): 
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton('↩️ Назад в меню')
+    markup.row(button1)
+
+    # Маппинг жанров на их названия
+    genre_map = {
+        'Фантастика': "Фантастика",
+        'Драма': "Драма",
+        'Ужасы': "Ужасы",
+        'Комедия': "Комедия"
+    }
+
+    genre_key = message.text.capitalize()  # Приводим текст сообщения к корректному формату
+    response = ""
+
+    if genre_key in genre_map:
+        genre_name = genre_map[genre_key]
+        response += f"Фильмы в жанре '{genre_name}':\n"
+        found = False
+        
+        for movie in my_dict.values():
+            if movie["Жанр"] == genre_name:
+                response += f"- {movie['Название']}: {movie['Описание']}\nСсылка: {movie['Ссылка']}\n"
+                found = True
+        
+        # Проверяем, были ли найдены фильмы
+        if not found:
+            response = f"Фильмы в жанре '{genre_name}' не найдены."
+
+    else:
+        response = "Пожалуйста, выберите другой жанр."
+
+    bot.send_message(message.chat.id, response, reply_markup=markup)
 
 
 
